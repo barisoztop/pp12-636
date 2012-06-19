87bd2252755f6cb00334b00eccd047c80c2e0b7e
t.sander83111 <t.sander83111@gmail.com>
2012-06-19 08:14:03 +0000

    added nueroal folder and classes

---
+++ MembranePony/src/neural/NeuralPredictor.java
@@ -0,0 +1,25 @@
+package neural;
+
+import interfaces.Predictor;
+import interfaces.Sequence;
+
+/**
+ * 
+ * @author tobias
+ */
+public class NeuralPredictor implements Predictor{
+    
+    
+    
+    @Override
+    public void predict(Sequence sequence) {
+        
+    }
+
+    @Override
+    public void train(Sequence[] trainingCases) {
+        
+        
+    }
+    
+}
---
+++ MembranePony/src/neural/NeuralPredictorFactory.java
@@ -0,0 +1,14 @@
+package neural;
+
+import interfaces.Predictor;
+import interfaces.PredictorFactory;
+
+
+public class NeuralPredictorFactory implements PredictorFactory{
+
+    @Override
+    public Predictor getInstance() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
