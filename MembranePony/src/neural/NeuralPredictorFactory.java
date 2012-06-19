bd1de594e56217e993255f4a42cfe648a9448560
t.sander83111@gmail.com <t.sander83111@gmail.com@47924109-9596-0a73-efbf-08a01d04f008>
2012-06-19 09:22:09 +0000

    created the basic structue of neuralpredictor and started to implement training method

--- MembranePony/src/neural/NeuralPredictorFactory.java
+++ MembranePony/src/neural/NeuralPredictorFactory.java
@@ -5,10 +5,54 @@ import interfaces.PredictorFactory;
 
 
 public class NeuralPredictorFactory implements PredictorFactory{
-
-    @Override
-    public Predictor getInstance() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    
+    private NeuralTask task;
+    private boolean configuration;
+    private int inputNeurons;
+    private int outputNeurons;
+    private String path;
+    
+    public NeuralPredictorFactory(NeuralTask task){
+        this.task = task;
+        this.configuration = false;
     }
     
+    @Override
+    public Predictor getInstance() {
+        
+        if(task == NeuralTask.TRAINING){
+            
+            if(this.configuration){
+                
+                return new NeuralPredictor(path, this.inputNeurons, this.outputNeurons);
+            
+            }else{
+                throw new IllegalStateException("Please use configuration method first"
+                        + "before using getInstance() method.");
+            }
+            
+        }else if(task == NeuralTask.PREDICTION){
+            
+            return new NeuralPredictor(path);
+        
+        }else{
+            
+            throw new IllegalStateException("NeuralPredictor Instance can not be created.");
+        }
+    }
+    
+    public void setTask(NeuralTask task){
+        this.task = task;
+    }
+    
+    public NeuralTask getActualTask(){
+        return this.task;
+    }
+    
+    public void configureNeuralNetwork(String pathToSaveNN, int inputNeurons, int outputNeurons){  
+        this.configuration = true;
+        this.inputNeurons = inputNeurons;
+        this.outputNeurons = outputNeurons;
+        this.path = pathToSaveNN;
+    }  
 }
