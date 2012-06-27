4ed1c3ed61057fed13f18428ea262c8f42113f7c
t.sander83111@gmail.com <t.sander83111@gmail.com@47924109-9596-0a73-efbf-08a01d04f008>
2012-06-27 16:47:42 +0000

    added generate training set and some needed methods to NeuralNetworkPrtedictor class

--- MembranePony/src/neural/NeuralPredictor.java
+++ MembranePony/src/neural/NeuralPredictor.java
@@ -1,11 +1,15 @@
 package neural;
 
 import data.AminoAcid;
+import data.SSE;
 import interfaces.Prediction;
 import interfaces.Predictor;
+import interfaces.Result;
 import interfaces.Sequence;
+import interfaces.SequencePosition;
 import interfaces.SlidingWindow;
 import java.io.File;
+import java.util.LinkedList;
 import org.neuroph.core.NeuralNetwork;
 import org.neuroph.core.learning.SupervisedTrainingElement;
 import org.neuroph.core.learning.TrainingSet;
@@ -36,7 +40,7 @@ public class NeuralPredictor implements Predictor{
         this.outputN = outputNeurons;
 
         //maybe here we could use a different subclass,
-        //eg maatrix multilayer perceptron, adaline, etc...
+        //eg matrix multilayer perceptron, adaline, etc...
         //here all input data n should be mapped onto a single perceptron
         //returning 1 for TMH and 0 for non TMH
         this.neuralNetwork = new Perceptron(inputNeurons, outputNeurons);
@@ -67,11 +71,7 @@ public class NeuralPredictor implements Predictor{
             throw new IllegalStateException("Neural Network was not loaded"
                     + "from harddisk. Please use load method to load it.");
         }
-        
-        Prediction p;
-        
-        
-        
+              
         return null;
     }
     
@@ -87,7 +87,7 @@ public class NeuralPredictor implements Predictor{
                     + "Training is not possible at the moment.");
         }
 
-        //first check if values of training cases corresond to configuration of network
+        //first check if values of training cases correspond to configuration of network
 
         //create a training set
         System.out.println("Generating training set...");
@@ -140,15 +140,34 @@ public class NeuralPredictor implements Predictor{
      * @param trainingSet
      */
     private void generateTrainingSet(Sequence[] trainingCases, TrainingSet<SupervisedTrainingElement> trainingSet){
+        
+        LinkedList<Double> trainingInputVector = new LinkedList<Double>();
+        LinkedList<Double> trainingOutputVector = new LinkedList<Double>();
+        
         for(Sequence s : trainingCases){
           
             SlidingWindow[] seqWindows = s.getWindows();
             
             for(SlidingWindow window : seqWindows){
                 
+                trainingInputVector.clear();
+                trainingOutputVector.clear();
+                
                 for(int i=0; i<window.getSequence().length; i++){
                     
-                }              
+                    SequencePosition position = window.getSequence()[i];
+                    
+                    //fill input vector
+                    trainingInputVector.add(mapAAsOntoDoubleValues(position.getAminoAcid()));
+                    trainingInputVector.add(position.getHydrophobicity());
+                    trainingInputVector.add(mapSSEontoDoubleValues(position.getSecondaryStructure()));
+                    
+                    //fill output vector
+                    trainingOutputVector.add(mapResultOntoDoubleValues(position.getRealClass()));
+                }
+                
+                //add a training element to the training set
+                trainingSet.addElement(new SupervisedTrainingElement(doubleLinkedListToDoubleArray(trainingInputVector),doubleLinkedListToDoubleArray(trainingOutputVector)));
             }          
         }
     }
@@ -200,15 +219,80 @@ public class NeuralPredictor implements Predictor{
     
     /**
      * 
+     * @param list
+     * @return 
+     */
+    private double[] doubleLinkedListToDoubleArray(LinkedList<Double> list){
+        
+        double[] output = new double[list.size()];
+        
+        for(int i=0; i<list.size(); i++){
+            output[i] = list.get(i);
+        }
+        
+        return output;
+    }
+    
+    /**
+     * 
+     * @param res
+     * @return 
+     */
+    public double mapResultOntoDoubleValues(Result res){
+        switch(res){
+            case INSIDE     : return 1.0;
+            case OUTSIDE    : return 2.0;
+            case TMH        : return 3.0;
+            case NON_TMH    : return 4.0;    
+                
+            default         : return 0.0;    
+        }
+    }
+    
+    /**
+     * 
      * @param aa
      * @return 
      */
     private double mapAAsOntoDoubleValues(AminoAcid aa){
         switch(aa){
-            case A : return 1.0;
-            
-            
+            case A  : return 1.0;
+            case C  : return 2.0;
+            case D  : return 3.0;
+            case E  : return 4.0;
+            case F  : return 5.0;
+            case G  : return 6.0;
+            case H  : return 7.0;
+            case I  : return 8.0;
+            case K  : return 9.0;
+            case L  : return 10.0;
+            case M  : return 11.0;
+            case N  : return 12.0;
+            case P  : return 13.0;
+            case Q  : return 14.0;
+            case R  : return 15.0;
+            case S  : return 16.0;
+            case T  : return 17.0;    
+            case V  : return 18.0;
+            case W  : return 19.0;
+            case Y  : return 20.0;    
+                
             default : return 0.0;
+        }
+    }
+    
+    /**
+     * 
+     * @param sse
+     * @return 
+     */
+    private double mapSSEontoDoubleValues(SSE sse){
+        switch(sse){
+            case Helix  : return 1.0;
+            case Coil   : return 2.0;
+            case Sheet  : return 3.0;
+                
+            default     : return 0.0;    
         }
     }
 }
