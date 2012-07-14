ff461b526e3fc2578cf0f05e71b5cb14c0bad1e9
r.h.greil <r.h.greil@gmail.com>
2012-07-14 09:47:17 +0000

    refactored structure

--- MembranePony/src/main/Evaluate.java
+++ MembranePony/src/main/Evaluate.java
@@ -6,27 +6,27 @@ import interfaces.Sequence;
 import java.io.File;
 import java.io.IOException;
 
-import markov.MarkovPredictorFactory;
+import predictor.markov.MarkovPredictorFactory;
 
 import data.Hydrophobicity;
 import evaluation.Evaluation;
 import evaluation.EvaluationResult;
 
 public class Evaluate {
-	
+
 	public static void main(String[] args) throws IOException {
-			
+
 		File dataFolder = new File("N:\\temp\\lean-dataset\\");
 		int table = Hydrophobicity.KYTE_DOOLITTLE;
-		
-		Sequence[] sequences = DataReader.readAll(dataFolder, table);
-		
+
+		Sequence[] sequences = DataReader.readAll(dataFolder, table, true);
+
 		Evaluation eval = new Evaluation(sequences, new MarkovPredictorFactory());
-		
+
 		EvaluationResult result = eval.evaluate();
-		
+
 		System.out.println(result);
-		
+
 	}
-	
+
 }
